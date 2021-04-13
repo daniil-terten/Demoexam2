bbd054c338918935c02cf9fe09b61143bffdaa8b
Daniil <daniil.terten@gmail.com>
2021-04-13 04:01:33 +0300

    V0.13 Добавлена функция удаления товара

--- src/com/GreatBritain/app/UI/ProductForm.java
+++ src/com/GreatBritain/app/UI/ProductForm.java
@@ -1,5 +1,6 @@
 package com.GreatBritain.app.UI;
 
+import com.GreatBritain.app.utils.DBHandler;
 import com.GreatBritain.app.utils.ProductTableManager;
 
 import javax.swing.*;
@@ -10,6 +11,7 @@ public class ProductForm  extends BaseForm{
     private JTable ProductTable;
     private JPanel panel;
     private JButton insertButton;
+    private JButton deleteButton;
 
     public ProductForm(){
         setContentPane(panel);
@@ -22,6 +24,15 @@ public class ProductForm  extends BaseForm{
                 InsertProductForm insertProductForm = new InsertProductForm(ProductTable);
                 insertProductForm.setVisible(true);
                 insertProductForm.pack();
+            }
+        });
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DBHandler.openConnection();
+                DBHandler.execQuery("DELETE FROM product WHERE ID = '"+ProductTable.getValueAt(ProductTable.getSelectedRow(), 0)+"'");
+                DBHandler.closeConnection();
+                ProductTableManager.refreshTable(ProductTable);
             }
         });
     }
